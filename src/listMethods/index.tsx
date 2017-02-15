7f6548537fa6ec3cbe5fe8bdb54e20a4de8e6d2f
matheus costa <mf.costa@live.com>
2017-02-15 20:37:24 -0200

    vai que vai

--- src/listMethods/index.tsx
+++ src/listMethods/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {  View, Image, Dimensions } from 'react-native'
 var { ImageZoom } = require('react-native-image-pan-zoom');
 var { Actions } = require('react-native-router-flux')
-var { Line, Svg } = require('react-native-svg');
+var { Line, Svg, G, Circle } = require('react-native-svg');
 import { teste, changeAddCodeFooterStatus } from './actions'
 import { connect } from 'react-redux'
 import { Account } from './../main/main'
@@ -15,14 +15,107 @@ import FooterTab from './components/footerTab'
 // var drawing = require('pngjs-draw');
 // var png = drawing(require('pngjs').PNG);
  const Graph = require('node-dijkstra')
+      const route = new Graph()
+      
+        const itemsList=[{
+          id:"A",
+          adjacentes:{ B:2, F:2 },
+          x: 40,
+          y: 35
+        },
+        {
+          id:"B",
+          adjacentes:{ A:2, F:4, C:5 },
+          x: 95,
+          y: 80
+        },
+        {
+          id:"C",
+          adjacentes:{ B:2, G:1 },
+          x: 450,
+          y: 22
+        },
+        {
+          id:"D",
+          adjacentes:{ G:1, F:2 },
+          x: 380,
+          y: 322
+        },
+        {
+          id:"E",
+          adjacentes:{ F:2 },
+          x: 45 ,
+          y: 255
+        },
+        {
+          id:"F",
+          adjacentes:{ E:2, A:2, B:4, D:2 },
+          x: 167,
+          y: 134
+        },
+        {
+          id:"G",
+          adjacentes:{ C:1, D:2 },
+          x: 455,
+          y: 57
+        }]
+
+const getPointCordetates = (id) => {
+  for(let key in itemsList){
+    console.log(itemsList[key])
+    if(itemsList[key].id ==id){
+      return{
+        x: itemsList[key].x,
+        y: itemsList[key].y,
+      }
+    } 
+  }
+  return{
+        x: 0,
+        y: 0,
+      }
+}
+
+
+const DrawLines = (item) => {
+  let keys: Array<string> = [];console.log(item.item)
+  for(let key in item.item.adjacentes){
+    
+     keys.push(key)
+  }
+  console.log(keys)
+  return(
+    <G>
+      {keys.map((key,index) =>{
+        console.log(getPointCordetates(key))
+        const cordenates = getPointCordetates(key)
+        return(
+              <G>
+                <Circle
+                  cx={cordenates.x}
+                  cy={cordenates.y}
+                  r="5"
+                  fill="pink"
+              />
+                  <Line
+              key={index}
+              x1={item.item.x}
+              y1={item.item.y}
+              x2={cordenates.x}
+              y2={cordenates.y}
+              stroke="red"
+              strokeWidth="2"
+                
+          />
+          </G>
+        )
+      })}
+    </G>
+  )
+
+}
+
 interface Appprops {
-    teste: Function,
-    showAddCodeFooter: string,
-    seconds: number,
-    accountList: Array<Account>,
-    changeAddCodeFooterStatus: Function,
-    timeStart: Function,
-    changeAccountCode: Function
 
 }
 
@@ -33,7 +126,9 @@ class listMethods extends React.Component<any,{}> {
     componentDidMount(): void {
 
  
-      const route = new Graph()
+
+
+
       
       route.addNode('A', { B:2, F:2 })
       route.addNode('B', { A:2, F:4, C:5 })
@@ -71,11 +166,54 @@ class listMethods extends React.Component<any,{}> {
 
     }
 
-    componentWillUnmount(): void {
-      this.props.timeStop()
-    }
 
     render(): JSX.Element {
+
+
+
+                const items=[{
+          id:"A",
+          adjacentes:{ B:2, F:2 },
+          x: 40,
+          y: 35
+        },
+        {
+          id:"B",
+          adjacentes:{ A:2, F:4, C:5 },
+          x: 95,
+          y: 80
+        },
+        {
+          id:"C",
+          adjacentes:{ B:2, G:1 },
+          x: 450,
+          y: 22
+        },
+        {
+          id:"D",
+          adjacentes:{ G:1, F:2 },
+          x: 380,
+          y: 322
+        },
+        {
+          id:"E",
+          adjacentes:{ F:2 },
+          x: 45 ,
+          y: 255
+        },
+        {
+          id:"F",
+          adjacentes:{ E:2, A:2, B:4, D:2 },
+          x: 167,
+          y: 134
+        },
+        {
+          id:"G",
+          adjacentes:{ C:1, D:2 },
+          x: 455,
+          y: 57
+        }]
+        
         return(
           <View>
 
@@ -88,26 +226,19 @@ class listMethods extends React.Component<any,{}> {
                           source={require('./../../images/base/graph.png')}/>
 
                       <View style={{position:'absolute'}}>
+
                         <Svg 
-                            height="580"
-                            width="400"
+                            height="800"
+                            width="800"
                         >
-                            <Line
-                                x1="2"
-                                y1="0"
-                                x2="580"
-                                y2="0"
-                                stroke="red"
-                                strokeWidth="2"
-                            />
-                            <Line
-                                x1="0"
-                                y1="0"
-                                x2="580"
-                                y2="400"
-                                stroke="red"
-                                strokeWidth="2"
-                            />
+                        {items.map((key,index)=> {
+                          console.log("-",key,index)
+                          return(
+                              <DrawLines item={key} key={index} />
+
+                          )
+                        })}
+                         
                         </Svg>
                       </View >
 
@@ -116,47 +247,15 @@ class listMethods extends React.Component<any,{}> {
 
       </View>
         )
-        //  return (
-        //   <View style={{flex: 1, flexDirection: 'column'}}>
-        //     <View >
-        //         <Navbar changeAddCodeFooterStatus={()=> this.props.changeAddCodeFooterStatus(true)} />
-        //     </View>
-        //     <View style={{flex: 1, flexDirection: 'column'}}>
-        //         <AccountCards 
-        //           updateAccountsCode={this.props.updateAccountsCode}
-        //           accountList={this.props.accountList} 
-        //           seconds={this.props.seconds} />
-        //     </View >
-        //     <View style={{flex: 1, flexDirection: 'column'}}>
-        //         <FooterTab 
-        //             openCamera={Actions.QrCodeReader} 
-        //             showAddCodeFooter={this.props.showAddCodeFooter}
-        //             changeAddCodeFooterStatus={() => this.props.changeAddCodeFooterStatus(false)} />
-        //     </View>
-        //  </View>
-        // );
     }
 }
 
 
 
 const mapStateToProps = (state,ownProps) => ({
-    showAddCodeFooter: state.listMethods.showAddCodeFooter,
-    seconds: state.listMethods.seconds,
-    accountList: state.main.accountList
   });
 
 const mapDispatchToProps = dispatch => ({
-  teste: () =>
-    dispatch(teste(true)),
-  changeAddCodeFooterStatus: (status:boolean) =>
-    dispatch(changeAddCodeFooterStatus(status)),
-  timeStart: () =>
-    dispatch(timeStart()),
-  timeStop: () =>
-    dispatch(timeStop()),
-  updateAccountsCode: () =>
-    dispatch(updateAccountsCode())
 
 });
 
